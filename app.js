652d69866c61ceb98399016094c7ac4ebb75be6f
u.yilmaz <unal.yilmaz123@outlook.com>
2021-12-18 22:27:34 +0300

    Patika | Nodejs Mongoose and Model added then posts displayed dynamically

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+
 const path = require('path');
 const ejs = require('ejs');
 
+const Photo = require('./models/Photo');
+
+const app = express();
+
+// Database connect
+mongoose.connect('mongodb://localhost/pcat-db',{useNewUrlParser: true,useUnifiedTopology: true});
 
 app.set('view engine','ejs');
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.render('index');
+
+app.get('/',async (req,res) => {
+
+    const photos = await Photo.find();
+    res.render('index',{photos});
 });
 
 app.get('/about',(req,res) => {
@@ -19,6 +31,11 @@ app.get('/about',(req,res) => {
 
 app.get('/addPhoto',(req,res) => {
     res.render('addPhoto');
+});
+
+app.post('/photos',async (req,res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 const port = 3000;
